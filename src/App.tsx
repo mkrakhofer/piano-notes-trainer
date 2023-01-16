3e83dc17931c8da1ea7c5b0aab18e69aba42d760
Michael Krakhofer <michael.krakhofer@gmail.com>
2023-01-16 22:20:49 +0100

    eslint warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,41 @@
 import React, {useEffect, useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  const [access, setAccess] = useState();
-  const [inputs, setInputs] = useState<Map<string, any>>(new Map());
+    const [access, setAccess] = useState();
+    const [inputs, setInputs] = useState<Map<string, any>>(new Map());
 
 
-  useEffect(() => {
-    let midi = null;  // global MIDIAccess object
-    function onMIDISuccess(midiAccess: any) {
-      console.log("MIDI ready!");
-      setAccess(midiAccess);
-      setInputs(midiAccess.inputs);
-      midi = midiAccess;  // store in the global (in real usage, would probably keep in an object instance)
+    useEffect(() => {
+        function onMIDISuccess(midiAccess: any) {
+            console.log("MIDI ready!");
+            setAccess(midiAccess);
+            setInputs(midiAccess.inputs);
+        }
+
+        function onMIDIFailure(msg: any) {
+            console.error(`Failed to get MIDI access - ${msg}`);
+        }
+
+        navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
+    }, [])
+
+    const onClick = () => {
+        console.log(access);
+        inputs.forEach((input) => {
+            console.log(input.name); /* inherited property from MIDIPort */
+            input.onmidimessage = (message: any) => {
+                console.log(message.data);
+            }
+        })
     }
 
-    function onMIDIFailure(msg: any) {
-      console.error(`Failed to get MIDI access - ${msg}`);
-    }
-
-    navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
-  }, [])
-
-  const onClick = () => {
-    console.log(access);
-    inputs.forEach((input) => {
-      console.log(input.name); /* inherited property from MIDIPort */
-      input.onmidimessage = (message: any) => {
-        console.log(message.data);
-      }
-    })
-  }
-
-  return (
-    <div className="App">
-      <button onClick={onClick}>Check Inputs</button>
-    </div>
-  );
+    return (
+        <div className="App">
+            <button onClick={onClick}>Check Inputs</button>
+        </div>
+    );
 }
 
 export default App;
