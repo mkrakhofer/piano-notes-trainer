a4c3d208b26bf8a0f9c7a1572f35c98d14c27730
Michael Krakhofer <michael.krakhofer@gmail.com>
2023-01-22 18:18:37 +0100

    prepare components for ottava alta/bassa

---
+++ src/components/ottava-alta/ottava-alta.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import "./ottava-alta.css";
+
+export const OttavaAlta = () => {};
---
+++ src/components/ottava-bassa/ottava-bassa.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import "./ottava-bassa.css";
+
+export const OttavaBassa = () => {};
